265e98ec6c4f6e6ab7bd749177f7f69de47a5f9a
AntoineFerrieux <antoine.ferrieux@wanadoo.fr>
2015-12-02 15:28:51 +0100

    Ajout Bundle 'Site'

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,7 @@ class AppKernel extends Kernel {
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new SiteBundle\SiteBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
