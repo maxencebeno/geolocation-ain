d73997932e59a33f9678dff58988e16fb712ca94
1s2vanessa <1s2vanessalehnert@gmail.com>
2016-01-06 16:37:09 +0100

    Header & footer

--- src/Geolocation/SiteBundle/Controller/IndexController.php
+++ src/Geolocation/SiteBundle/Controller/IndexController.php
@@ -4,10 +4,10 @@ namespace Geolocation\SiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class IndexController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('SiteBundle:Default:index.html.twig');
+        return $this->render('SiteBundle:Index:index.html.twig');
     }
 }
