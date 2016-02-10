d3b4df971ce7866f2a4e8d5e66c8fbb79f084f64
maxencebeno <maxence.beno@gmail.com>
2016-02-10 11:54:18 +0100

    ajax

--- src/Geolocation/UserBundle/Resources/public/js/ajax_form.js
+++ src/Geolocation/UserBundle/Resources/public/js/ajax_form.js
@@ -12,6 +12,44 @@ $(document).ready(function () {
 
     });
 
+    $('#division').change(function () {
+        var sectionId = $('#sections').val();
+        var divisionId = $(this).val();
+
+        getGroupe(sectionId, divisionId, $('.groupe'));
+
+    });
+
+    $('#groupe').change(function () {
+        var sectionId = $('#sections').val();
+        var divisionId = $('#division').val();
+        var groupId = $('#groupe').val();
+
+        getClasse(sectionId, divisionId, groupId, $('.classe'));
+
+    });
+
+    $('#classe').change(function () {
+        var sectionId = $('#sections').val();
+        var divisionId = $('#division').val();
+        var groupId = $('#groupe').val();
+        var classeId = $('#classe').val();
+
+        getCategorie(sectionId, divisionId, groupId, classeId, $('.categorie'));
+
+    });
+
+    $('#categorie').change(function () {
+        var sectionId = $('#sections').val();
+        var divisionId = $('#division').val();
+        var groupId = $('#groupe').val();
+        var classeId = $('#classe').val();
+        var categorieId = $('#categorie').val();
+
+        getSousCategorie(sectionId, divisionId, groupId, classeId, categorieId, $('.sous-categorie'));
+
+    });
+
     function getDivision(sectionId, division) {
         $.ajax({
             url: '/app_dev.php/ajax/getDivision?id=' + sectionId,
@@ -20,6 +58,54 @@ $(document).ready(function () {
                     division.append("<option value='" + data[i].id + "'>" + data[i].libelle + "</option>");
                 }
                 $('.division-form').removeClass('hide');
+            }
+        });
+    }
+
+    function getGroupe(sectionId, divisionId, groupe) {
+        $.ajax({
+            url: '/app_dev.php/ajax/getGroupe?sectionid=' + sectionId + '&divisionid=' + divisionId,
+            success: function (data) {
+                for (var i = 0; i < data.length; i++) {
+                    groupe.append("<option value='" + data[i].id + "'>" + data[i].libelle + "</option>");
+                }
+                $('.groupe-form').removeClass('hide');
+            }
+        });
+    }
+
+    function getClasse(sectionId, divisionId, groupeId, classe) {
+        $.ajax({
+            url: '/app_dev.php/ajax/getGroupe?sectionid=' + sectionId + '&divisionid=' + divisionId + '&groupeid=' + groupeId,
+            success: function (data) {
+                for (var i = 0; i < data.length; i++) {
+                    classe.append("<option value='" + data[i].id + "'>" + data[i].libelle + "</option>");
+                }
+                $('.classe-form').removeClass('hide');
+            }
+        });
+    }
+
+    function getCategorie(sectionId, divisionId, groupeId, classeId, categorie) {
+        $.ajax({
+            url: '/app_dev.php/ajax/getGroupe?sectionid=' + sectionId + '&divisionid=' + divisionId + '&groupeid=' + groupeId + '&classeid=' + classeId,
+            success: function (data) {
+                for (var i = 0; i < data.length; i++) {
+                    categorie.append("<option value='" + data[i].id + "'>" + data[i].libelle + "</option>");
+                }
+                $('.categorie-form').removeClass('hide');
+            }
+        });
+    }
+
+    function getSousCategorie(sectionId, divisionId, groupeId, classeId, categorieId, souscategorie) {
+        $.ajax({
+            url: '/app_dev.php/ajax/getGroupe?sectionid=' + sectionId + '&divisionid=' + divisionId + '&groupeid=' + groupeId + '&classeid=' + classeId + '&categorieid=' + categorieId,
+            success: function (data) {
+                for (var i = 0; i < data.length; i++) {
+                    souscategorie.append("<option value='" + data[i].id + "'>" + data[i].libelle + "</option>");
+                }
+                $('.sous-categorie-form').removeClass('hide');
             }
         });
     }
