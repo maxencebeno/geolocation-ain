1a2cc0df12e910af11a2ce7d26c95bf60d6661dd
Vanessa Lehnert <1s2vanessalehnert@gmail.com>
2016-04-04 14:57:20 +0200

    Google Maps

--- src/Geolocation/SiteBundle/Controller/IndexController.php
+++ src/Geolocation/SiteBundle/Controller/IndexController.php
@@ -14,7 +14,7 @@ class IndexController extends Controller {
     public function indexAction() {
         $em = $this->getDoctrine()->getManager();
         $sections = $em->getRepository('GeolocationAdminBundle:Section')
-            ->findAll();
+                ->findAll();
 
         return $this->render('SiteBundle:Index:index.html.twig', ['sections' => $sections]);
     }
@@ -65,17 +65,31 @@ class IndexController extends Controller {
         $em = $this->getDoctrine()->getManager();
         $array = $em->getRepository('GeolocationAdminBundle:User')
                 ->findByEnabled(1);
+
+        $ressources = array();
+        foreach ($array as $user) {
+            $besoin = $em->getRepository('GeolocationAdminBundle:Ressources')
+                    ->findOneBy(array('user' => $user->getId(), 'besoin'=> true));
+            $proposition =  $em->getRepository('GeolocationAdminBundle:Ressources')
+                    ->findOneBy(array('user' => $user->getId(), 'besoin'=> false));
+            $ressources[$user->getId()] = 
+                    array('besoin' => $besoin,
+                        'proposition'=>$proposition,
+                        'user'=>$user
+                           );
+        }
         
+        $ignoredAttributes = array('user');
         $normalizer = new GetSetMethodNormalizer();
-        $serializer = new Serializer(array($normalizer), array('array' => new JsonEncoder()));
-        
-        $jsonContent = $serializer->serialize($array, 'json');
+        $normalizer->setIgnoredAttributes($ignoredAttributes);
+        $serializer = new Serializer(array($normalizer), array('ressources' => new JsonEncoder()));
+        $jsonContent = $serializer->serialize($ressources, 'json');  
         
         $response = new Response($jsonContent);
         $response->headers->set('Content-Type', 'application/json');
         return $response;
     }
-    
+
     public function getCodepostalFromCityAction(Request $request) {
         if ($request->isXmlHttpRequest()) {
             $array = $this->getDoctrine()
@@ -87,21 +101,6 @@ class IndexController extends Controller {
             $response->headers->set('Content-Type', 'application/json');
             return $response;
         }
-    }
-    
-    public function getRessourcesByUserIdAction(Request $request){
-         $em = $this->getDoctrine()->getManager();
-         $ressources = $em->getRepository('GeolocationAdminBundle:Ressources')
-                ->findBy(array('user' => $request->query->get('id')));
-         
-        $normalizer = new GetSetMethodNormalizer();
-        $serializer = new Serializer(array($normalizer), array('ressources' => new JsonEncoder()));
-        
-        $jsonContent = $serializer->serialize($ressources, 'json');
-        
-        $response = new Response($jsonContent);
-        $response->headers->set('Content-Type', 'application/json');
-         return $response;
     }
 
 }
--- src/Geolocation/SiteBundle/Resources/public/js/mapbuilder.js
+++ src/Geolocation/SiteBundle/Resources/public/js/mapbuilder.js
@@ -1,5 +1,4 @@
-
-
+//initialisation Google Maps
 var latlngCentre = new google.maps.LatLng(46.204960, 5.225797);
 var options = {
     center: latlngCentre,
@@ -8,57 +7,76 @@ var options = {
 };
 var carte = new google.maps.Map(document.getElementById("map"), options);
 
-
+//initialisation des variables de la carte
 var infowindow = new Array();
+var latlng = new Array();
+var markers = new Array();
+var contentString = "";
+
 //chargement des données 1 seule fois
 google.maps.event.addListenerOnce(carte, 'idle', function () {
-    var latlng = new Array();
-    var markers = new Array();
-    var contentString = "";
-
     //appel de l'action pour charger les markers
     $.ajax({
         url: baseUrl + 'json/markers',
         success: function (data) {
-            var i;
-            var index = 0;
-            for (i in data) {
-                console.log(data[i]);
-                latlng.push(new google.maps.LatLng(data[i].latitude, data[i].longitude));
-                markers.push(new google.maps.Marker({
-                    position: latlng[index],
-                    map: carte,
-                    title: 'Test ' + index
-                }));
-                contentString =
-                        '<div id="content">' +
-                        '<h2>' + data[i].nom + '</h2>' +
-                        data[i].adresse + '<br>' +
-                        data[i].codePostal + ' '+ data[i].ville + '<br>'+
-                        '<a href = "' + baseUrl + 'details\\' + data[i].id + '">Plus d\'informations<a>' +
-                        '</div>';
-
-                infowindow[index] = new google.maps.InfoWindow({
-                    content: contentString
-                });
-
-                google.maps.event.addListener(markers[index], 'click', function (index) {
-                    return function () {
-                        closeWindowInfos(); //fermer toutes les infoWindows ouvertes
-                        infowindow[index].open(carte, markers[index]);
-                    }
-                }(index));
-
-                index++;
-
-            }
-            //alert(url);
-
-
+            clearMarker();
+            initMarker(data);
         }
     });
 });
 
+//vider la liste des markers
+function clearMarker() {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
+    markers = [];
+    latlng = [];
+}
+
+//initialiser les marker en fonctions de la requete ajax
+function initMarker(data) {
+    var i;
+    var index = 0;
+    for (i in data) {
+        latlng.push(new google.maps.LatLng(data[i].user.latitude, data[i].user.longitude));
+        markers.push(new google.maps.Marker({
+            position: latlng[index],
+            map: carte,
+            title: 'Test ' + index
+        }));
+        contentString =
+                '<div id="content">' +
+                '<h2>' + data[i].user.nom + '</h2>' +
+                data[i].user.adresse + '<br>' +
+                data[i].user.codePostal + ' ' + data[i].user.ville + '<br>' +
+                '<h3>Ressources</h3>';
+
+        if (data[i].besoin !== null) {
+            contentString += '<h4>Besoin</h4><p>' + data[i].besoin.cpf.groupe.libelle + '</p>';
+        }
+
+        if (data[i].proposition !== null) {
+            contentString += '<h4>Proposition</h4><p>' + data[i].proposition.cpf.groupe.libelle + '</p>';
+        }
+
+        contentString += '<a href = "' + baseUrl + 'details\\' + data[i].id + '">Plus d\'informations<a>' +
+                '</div>';
+        infowindow[index] = new google.maps.InfoWindow({
+            content: contentString
+        });
+
+        google.maps.event.addListener(markers[index], 'click', function (index) {
+            return function () {
+                closeWindowInfos(); //fermer toutes les infoWindows ouvertes
+                infowindow[index].open(carte, markers[index]);
+            }
+        }(index));
+        index++;
+    }
+}
+
+//fermeture de toutes les infosWindows
 function closeWindowInfos() {
     for (var i = 0; i < infowindow.length; i++)
     {
@@ -66,6 +84,7 @@ function closeWindowInfos() {
     }
 }
 
+//liaison d'un marker avec une info window
 function bindInfoWindow(marker, content) {
     google.maps.event.addListener(marker, 'click', function () {
         infowindow.setContent(content);
@@ -76,7 +95,6 @@ function bindInfoWindow(marker, content) {
 $(document).ready(function () {
 
     function initialize() {
-
         var latlngCentre = new google.maps.LatLng(46.204960, 5.225797);
         var options = {
             center: latlngCentre,
@@ -86,55 +104,5 @@ $(document).ready(function () {
     }
 
     initialize();
-    /*var latlng2 = new Array();
-     var markers = new Array();
-     var contentString = "";
-     latlng2.push(new google.maps.LatLng(45.7578, 4.8400));
-     latlng2.push(new google.maps.LatLng(45.771633, 4.890169));
-     for (index = 0; index < latlng2.length; ++index) {
-     
-     markers.push(new google.maps.Marker({
-     position: latlng2[index],
-     map: carte,
-     title: 'Test ' + index
-     }));
-     contentString =
-     '<div id="content">' +
-     '<h2>Nom de l\'entreprise '+ index+'</h2>' +
-     'adresse & contact' +
-     'lien vers la fiche entreprise' +
-     '</div>';
-     
-     infowindow[index] = new google.maps.InfoWindow({
-     content: contentString
-     });
-     
-     google.maps.event.addListener(markers[index], 'click', function (index) {
-     return function () {
-     closeWindowInfos(); //fermer toutes les infoWindows ouvertes
-     infowindow[index].open(carte, markers[index]);
-     }
-     }(index));
-     }
-     
-     }
-     
-     function closeWindowInfos() {
-     for (var i = 0; i < infowindow.length; i++)
-     {
-     infowindow[i].close();
-     }
-     }
-     
-     function bindInfoWindow(marker, content) {
-     google.maps.event.addListener(marker, 'click', function () {
-     infowindow.setContent(content);
-     infowindow.open(carte, marker);
-     alert(content);
-     });
-     }*/
-
-
-
 });
 
