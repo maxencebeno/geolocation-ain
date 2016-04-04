202b3d9e44dc8af65f96693a161e49b72e572d55
Vanessa Lehnert <1s2vanessalehnert@gmail.com>
2016-04-04 09:42:58 +0200

    Error __e3

--- src/Geolocation/SiteBundle/Resources/public/js/mapbuilder.js
+++ src/Geolocation/SiteBundle/Resources/public/js/mapbuilder.js
@@ -1,15 +1,53 @@
- var latlngCentre = new google.maps.LatLng(46.204960, 5.225797);
-        var options = {
-            center: latlngCentre,
-            zoom: 12,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
+var latlngCentre = new google.maps.LatLng(46.204960, 5.225797);
+var options = {
+    center: latlngCentre,
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+};
 var carte = new google.maps.Map(document.getElementById("map"), options);
 
-$(document).ready(function () {
-    var carte;
+//chargement des données 1 seule fois
+google.maps.event.addListenerOnce(carte, 'idle', function () {
+    var latlng = new Array();
+    var markers = new Array();
+    var contentString = "";
     var infowindow = new Array();
 
+    //appel de l'action pour charger les markers
+    $.ajax({
+        url: '/app_dev.php/json/markers',
+        success: function (data) {
+            var i;
+            var index = 0;
+            for (i in data) {
+                console.log(data[i]);
+                latlng.push(new google.maps.LatLng(data[i].latitude, data[i].longitude));
+                markers.push(new google.maps.Marker({
+                    position: latlng[index],
+                    map: carte,
+                    title: 'Test ' + index
+                }));
+                contentString =
+                        '<div id="content">' +
+                        '<h2>Nom de l\'entreprise ' + data[i].name + '</h2>' +
+                        'adresse & contact' +
+                        'lien vers la fiche entreprise' +
+                        '</div>';
+
+                infowindow[index] = new google.maps.InfoWindow({
+                    content: contentString
+                });
+                index++;
+
+            }
+            //alert(url);
+
+
+        }
+    });
+});
+
+$(document).ready(function () {
 
     function initialize() {
 
@@ -19,28 +57,10 @@ $(document).ready(function () {
             zoom: 12,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-
-        var latlng = new Array();
-        var markers = new Array();
-        var contentString = "";
-      
-        url = "/app_dev.php/json/markers";
-          //alert(url);
-        //chargement des données 1 seule fois
-        google.maps.event.addListenerOnce(carte, 'idle', function () {
-            //appel de l'action pour charger les markers
-            $.ajax({
-                url: '/app_dev.php/json/markers',
-                success: function (data) {
-                        alert(data.lenght);
-
-                }
-            });
-        });
     }
-    
+
     initialize();
-    
+
 
     /*var latlng2 = new Array();
      var markers = new Array();
