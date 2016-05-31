aa06f7fb42fa763a0e528e506d42de42c6270ef2
maxencebeno <maxence.beno@gmail.com>
2016-05-31 09:38:10 +0200

    listing

--- src/Geolocation/SiteBundle/Controller/ListeInscritsController.php
+++ src/Geolocation/SiteBundle/Controller/ListeInscritsController.php
@@ -3,16 +3,21 @@
 namespace Geolocation\SiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Translation\Translator;
+use Symfony\Component\HttpFoundation\Request;
 
-class ListeInscritsController extends Controller {
+class ListeInscritsController extends Controller
+{
 
-    public function listeInscritsAction() {
+    public function listeInscritsAction(Request $request)
+    {
+        $translator = new Translator($request->getLocale());
         $em = $this->getDoctrine()->getManager();
         $array = [];
-        
+
         $piliers = $em->getRepository('GeolocationAdminBundle:Pilier')
             ->findBy([], ['nom' => 'ASC']);
-        
+
         foreach ($piliers as $pilier) {
             if (!isset($array[$pilier->getId()])) {
                 $array[$pilier->getId()] = [];
@@ -26,14 +31,19 @@ class ListeInscritsController extends Controller {
             if (count($entreprises) > 0) {
                 $array[$pilier->getId()] = ['entreprises' => $entreprises, 'pilier' => $pilier->getNom()];
             } else {
-                $array[$pilier->getId()] = ['entreprises' => 'entreprise.no_entreprise', 'pilier' => $pilier->getNom()];
+                $array[$pilier->getId()] = ['entreprises' => null, 'pilier' => $pilier->getNom()];
             }
         }
-        
-        
-      //  var_dump($entreprises);
-        return $this->render('SiteBundle:ListeInscrits:listeinscrits.html.twig', 
-                array('datas'=>$array)
+
+        $entreprises = $em->getRepository('GeolocationAdminBundle:Adresse')
+            ->findBy(['pilier' => null]);
+
+        $array['nopilier'] = ['entreprises' => $entreprises, 'pilier' => $translator->trans('entreprise.no_pilier', [], 'GeolocationUserBundle')];
+
+
+        //  var_dump($entreprises);
+        return $this->render('SiteBundle:ListeInscrits:listeinscrits.html.twig',
+            array('datas' => $array)
         );
     }
 
