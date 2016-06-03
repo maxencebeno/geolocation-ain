f03ce303dca8aa4fe55c6dc5f0fc674baa282b9b
maxencebeno <maxence.beno@gmail.com>
2016-06-03 12:03:36 +0200

    Modal suppression

---
+++ src/Geolocation/UserBundle/Controller/SuppressionController.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maxencebeno
+ * Date: 03/06/2016
+ * Time: 11:50
+ */
+
+namespace Geolocation\UserBundle\Controller;
+
+
+use Geolocation\AdminBundle\Entity\Suppression;
+use Geolocation\AdminBundle\Form\SuppressionType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class SuppressionController extends Controller
+{
+    public function demandeSuppressionAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $suppression = new Suppression();
+        $form = $this->createForm(new SuppressionType(), $suppression);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em->persist($suppression);
+            $em->flush();
+
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Nouvelle demande de Suppression de compte')
+                ->setFrom($suppression->getUser()->getEmail())
+                ->setTo('georic.ain@gmail.com')
+                ->setBody(
+                    $this->renderView(
+                        '@GeolocationUser/Email/delete.html.twig',
+                        array('contact' => $suppression)
+                    ),
+                    'text/html'
+                );
+            $this->get('mailer')->send($message);
+
+            $this->get('session')->getFlashBag()->add('success', $this->get('translator')->trans('suppression.form.success', [], 'suppression'));
+
+            return $this->redirectToRoute('user_ask_delete_account');
+        }
+
+        return $this->render('GeolocationUserBundle:Suppression:suppression.html.twig', ['form' => $form->createView()]);
+    }
+}
