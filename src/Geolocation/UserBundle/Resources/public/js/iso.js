21765e1437e7cdbe69c6812397b33e0e6165249b
Vanessa Lehnert <1s2vanessalehnert@gmail.com>
2016-06-06 09:38:01 +0200

    Doc du JS pour les ISO

--- src/Geolocation/UserBundle/Resources/public/js/iso.js
+++ src/Geolocation/UserBundle/Resources/public/js/iso.js
@@ -8,7 +8,9 @@ $(document).ready(function () {
     //première initialisation des checkbox : utiliser notamment dans la mise à jour des sites de production
     initCheckbox();
 
-    //gestion des interactions avec les checkbox pour afficher les choix de certification, et la date si certifié
+    /*
+     * gestion des interactions avec les checkbox pour afficher les choix de certification, et la date si certifié
+     */
     $(checkbox).on('change', function () {
         if ($(this).is(':checked')) {
             var id = $(this).attr("value");
@@ -23,14 +25,21 @@ $(document).ready(function () {
         }
     });
 
-    
+ 
+    /*
+     * gestion des interaction avec les radio button
+     */
     $("input[type=radio].certifie").on('change', function () {
         //récupération de la valeur de la checkbox
         var checkId =$(this).parent("label").parent(".certification").prev("label").children().attr("value");
         dateCertification($(this), checkId);
     });
     
-    //initialisation des checkbox avant interaction
+    
+    /*
+     * Initialisation des checkbox avant interaction
+     * 
+     */
     function initCheckbox() {
         $(checkbox).each(function () {
             if ($(this).is(':checked')) {
@@ -40,38 +49,53 @@ $(document).ready(function () {
         });
     }
 
-    //initialisation des radio button avant interaction
+    /*
+     * initialisation des radio button avant interaction
+     */
     function initRadioButton() {
         $('input[type=radio]').each(function () {
             if ($(this).is(':checked')) {
-                var check =$(this).parent().parent().next("label").children();
-                var id = $(check).attr("value");
-                certificationRadio($(check), id);
+                //récupération du label du container précédent pour récupérer par la suite sa valeur qui servira
+                //d'id pour la fonction certificationRadio
+                var checkbox =$(this).parent().parent().next("label").children(); 
+                var id = $(checkbox).attr("value");
+                certificationRadio($(checkbox), id);
             }
         });
     }
     
-    //affiche les boutons radio du choix de certification (en cours ou certifié)
-    function certificationRadio(box, id) {
-        if ($(box).parent().text().trim() !== "Autre") {
+    
+    /*
+     * Affiche les boutons radio du choix de certification (en cours ou certifié) 
+     * @param {type} checkbox 
+     * @param {type} id : id qui est la valeur de la checkbox
+     */
+    function certificationRadio(checkbox, id) {
+        //si c'est un choix différent de "Autre" qui est fait
+        if ($(checkbox).parent().text().trim() !== "Autre") {
 
             $('     <label> Etes-vous certifié ? ' +
                     '<input type="radio" name="certifie-' + id + '" value="oui" class="certifie" > Oui' +
                     '<input type="radio" name="certifie-' + id + '" class="certifie" value="en-cours" checked> En cours de certification' +
                     '</label>' +
-                    '<div class="date_certif"></div>').appendTo($(box).parent().siblings('.certification'));
+                    '<div class="date_certif"></div>').appendTo($(checkbox).parent().siblings('.certification'));
         } else {
             $('     <label>Entrer un code ISO *<input type="text" name="other" id="other" placeholer="Entrer un code ISO" required /></label>' +
                     '<label> Etes-vous certifié ? ' +
                     '<input type="radio" name="certifie-other" class="certifie" value="oui"> Oui' +
                     '<input type="radio" name="certifie-other" class="certifie" value="en-cours" checked> En cours de certification' +
                     '</label>' +
-                    '<div class="date_certif"></div>').appendTo($(box).parent().siblings('.certification'));
+                    '<div class="date_certif"></div>').appendTo($(checkbox).parent().siblings('.certification'));
             id = "other";
         }
     }
 
-    //Affiche le champ pour entrer la date de certification
+    
+    /*
+     * Affiche le champ pour entrer la date de certification
+     * @param {type} radio
+     * @param {type} id
+     */
     function dateCertification(radio, id) {
         if ($(radio).val().trim() === "oui") {
             $('     <label> Depuis quand ? ' +
@@ -83,11 +107,12 @@ $(document).ready(function () {
                 format: "d/m/Y"
             });
         } else {
+            //suppression de champ de date certification
             $(radio).parent().siblings('.date_certif').empty();
         }
     }
 
-    //Supprimer le contenu de la certification
+    //Supprimer la totalité du contenu de la certification
     function viderCertification(box) {
         $(box).parent().siblings('.certification').empty();
     }
